f150d05248c0fae42b1d1ba59185020fe24952f1
hoshizaki <kant.cmd@gmail.com>
2022-09-17 00:53:01 +0700

    [add status display]

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
-const Home = () => {
+const Home = async () => {
+    const response = await fetch(`http://${import.meta.env.VITE_BACKEND}:7700/`)
+    const { data, status, errors } = await response.json()
     return (
-    <div>
-        <h2>Welcome to home</h2>
-        <h3>We're working now.</h3>
-        <Link to="/post">posts</Link>
-    </div>
+        <div>
+            <h2>Welcome to home</h2>
+            <h3>We're working now.</h3>
+            <h3>Status: {status}</h3>
+            
+
+            <Link to="/post">posts</Link>
+        </div>
     );
 }
 
