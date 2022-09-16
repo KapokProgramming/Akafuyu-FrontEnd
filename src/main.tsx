9f324bd4481a2f4cb78f8ad202ca12e9a0986cd8
hoshizaki <kant.cmd@gmail.com>
2022-09-16 22:32:32 +0700

    [:star: fix]

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import Post from './pages/Post/Post'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/post" element= {<Post />}></Route>
+      <Route path="/" element={<Post />}></Route>
+      {/* <Route path="/post" element= {<Post />}></Route> */}
     </Routes>
   </BrowserRouter>
 )
